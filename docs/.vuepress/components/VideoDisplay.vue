91ef3a8342eb737a120a19c8975e31c9211e9a43
Noah <noahbresler2002@gmail.com>
2020-04-01 04:06:05 -0500

    feat(VideoDisplay.vue): create a video display component

---
+++ docs/.vuepress/components/VideoDisplay.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="video-display flex justify-center">
+    <video width="100%" controls loop>
+      <source :src="src" type="video/mp4" />
+      Your browser does not support the video tag 😢.
+    </video>
+  </div>
+</template>
+<script>
+export default {
+  props: ["src"],
+  computed: {},
+  mounted: function() {}
+};
+</script>
+<style scoped>
+.video-display {
+  padding: 1em 0;
+}
+</style>
