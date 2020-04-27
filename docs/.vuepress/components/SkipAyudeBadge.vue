05e974b812bdf6c6a312a33819ab6ea994890a66
Noah <noahbresler2002@gmail.com>
2020-04-27 02:00:24 -0500

    refactor: extend HideAyudeWrappe and SkipAyudeBadge by IsUsingDriveEncodersComponent

--- docs/.vuepress/components/SkipAyudeBadge.vue
+++ docs/.vuepress/components/SkipAyudeBadge.vue
@@ -3,43 +3,25 @@
     skip me
   </div>
 </template>
-<script>
-export default {
+<script lang="ts">
+import Vue from "vue";
+import IsUsingDriveEncodersComponent from "./IsUsingDriveEncodersComponent";
+
+export default Vue.extend({
+  extends: IsUsingDriveEncodersComponent,
   props: ["skipIfDriveEncoders"],
   data() {
     return {
-      hidden: true
+      hidden: true,
     };
   },
-  mounted() {
-    if (localStorage.isUsingDriveEncoders) {
-      if (this.skipIfDriveEncoders)
-        this.hidden = !(
-          localStorage.isUsingDriveEncoders === "true" ||
-          localStorage.isUsingDriveEncoders
-        );
-      else
-        this.hidden =
-          localStorage.isUsingDriveEncoders === "true" ||
-          localStorage.isUsingDriveEncoders;
-    }
-
-    document.addEventListener(
-      "isUsingDriveEncodersChanged",
-      this.onIsUsingDriveEncoderChanged,
-      false
-    );
-  },
-  beforeDestroy() {
-    document.removeEventListener(this.onIsUsingDriveEncoderChanged);
-  },
   methods: {
-    onIsUsingDriveEncoderChanged(e) {
+    onIsUsingDriveEncoderChanged(e: CustomEvent) {
       if (this.skipIfDriveEncoders) this.hidden = !e.detail;
       else this.hidden = e.detail;
-    }
-  }
-};
+    },
+  },
+});
 </script>
 <style lang="stylus" scoped>
 .skipme {
