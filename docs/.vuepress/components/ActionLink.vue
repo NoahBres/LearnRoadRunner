a46c188cfca3ee71fe7bd18d46c1fd848908bfb6
Noah <noahbresler2002@gmail.com>
2020-04-30 06:50:49 -0500

    feat(ActionLink.vue): move styles away from action link

--- docs/.vuepress/components/ActionLink.vue
+++ docs/.vuepress/components/ActionLink.vue
@@ -1,36 +1,19 @@
 <template>
-  <div class="action-button flex justify-center align-center" :style="style">
+  <div class="action-link flex justify-center align-center" :style="style">
     <a v-bind:href="url" class="border-red-500 bg-gray-300">
       <slot />
     </a>
   </div>
 </template>
-<script>
-export default {
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
   props: ["url", "margin"],
   computed: {
     style() {
       return `margin: ${this.margin} 0`;
-    }
-  }
-};
+    },
+  },
+});
 </script>
-<style lang="stylus" scoped>
-.action-button a {
-  background: $accentColor;
-  padding: 1em 1.2em;
-
-  border-radius: 0.2em;
-  border-bottom: 0.1em solid darken($accentColor, 30%)
-
-  color: #fff;
-  font-size: 1.2em;
-
-  transition: background-color 0.1s ease
-}
-
-.action-button a:hover {
-  background-color: lighten($accentColor, 10%)
-  text-decoration: none;
-}
-</style>
