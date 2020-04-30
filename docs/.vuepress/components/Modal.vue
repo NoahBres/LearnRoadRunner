7d6a747237c34cb41a3f028fe072e955918a1e59
Noah <noahbresler2002@gmail.com>
2020-04-30 06:51:04 -0500

    feat(Modal.vue): create modal

---
+++ docs/.vuepress/components/Modal.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <transition name="fade">
+      <div
+        v-if="isOpen"
+        class="z-30 bg-gray-900 opacity-75"
+        @click="closeModal"
+      ></div>
+    </transition>
+    <div
+      v-if="isOpen"
+      class="z-40 pointer-events-none flex justify-center align-center fixed top-0 right-0 bottom-0 left-0"
+    >
+      <div class="bg-white pointer-events-auto"></div>
+    </div>
+  </div>
+</template>
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  props: {
+    isOpen: {
+      type: Boolean,
+      default: false,
+    },
+    closeModal: Function,
+  },
+  data() {
+    return {};
+  },
+  methods: {},
+});
+</script>
+<style lang="stylus" scoped>
+.fade-enter-active, .fade-leave-active
+  transition opacity 250ms
+
+.fade-enter, .fade-leave-to
+  opacity 0
+</style>
