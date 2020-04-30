69fe4c9a7cf8320e7e84ff1d115ad48c40f9ec64
Noah <noahbresler2002@gmail.com>
2020-04-30 06:51:24 -0500

    feat(ModalWrapper.vue): create modal wrapper

---
+++ docs/.vuepress/components/ModalWrapper.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <slot name="button" :openModal="openModal" :closeModal="closeModal" />
+    <slot name="modal" :isOpen="isModalOpened" :closeModal="closeModal" />
+  </div>
+</template>
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  data() {
+    return {
+      isModalOpened: false,
+    };
+  },
+  methods: {
+    openModal() {
+      this.isModalOpened = true;
+    },
+    closeModal() {
+      this.isModalOpened = false;
+    },
+  },
+});
+</script>
