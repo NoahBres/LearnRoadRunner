829d1d49f72e807d93153226d433706516cf8d2b
Noah <noahbresler2002@gmail.com>
2020-05-02 04:16:25 -0500

    feat(modal.vue): add modal in transition

--- docs/.vuepress/components/Modal.vue
+++ docs/.vuepress/components/Modal.vue
@@ -3,16 +3,20 @@
     <transition name="fade">
       <div
         v-if="isOpen"
-        class="z-30 bg-gray-900 fixed top-0 bottom-0 left-0 right-0 bg-opacity-75"
+        class="z-30 bg-gray-900 fixed top-0 bottom-0 left-0 right-0 bg-opacity-85"
         @click="closeModal"
       ></div>
     </transition>
-    <div
-      v-if="isOpen"
-      class="z-40 pointer-events-none flex justify-center items-center fixed top-0 right-0 bottom-0 left-0"
-    >
-      <div class="bg-white pointer-events-auto"></div>
-    </div>
+    <transition name="modal-in">
+      <div
+        v-if="isOpen"
+        class="z-40 pointer-events-none flex justify-center items-center fixed top-0 right-0 bottom-0 left-0"
+      >
+        <div class="bg-white pointer-events-auto p-0 rounded">
+          <slot />
+        </div>
+      </div>
+    </transition>
   </div>
 </template>
 <script lang="ts">
@@ -34,8 +38,15 @@ export default Vue.extend({
 </script>
 <style lang="stylus" scoped>
 .fade-enter-active, .fade-leave-active
-  transition opacity 250ms
+  transition opacity 150ms ease-in-out
 
 .fade-enter, .fade-leave-to
   opacity 0
+
+.modal-in-enter-active, .modal-in-leave-active
+  transition transform 150ms ease-in-out, opacity 150ms ease-in-out
+
+.modal-in-enter, .modal-in-leave-to
+  transform scale(0.7)
+  opacity 0.5
 </style>
