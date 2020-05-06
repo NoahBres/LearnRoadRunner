f5b1112d3e2f169d6ac4a3ae7e2faa74ac0cef83
Noah <noahbresler2002@gmail.com>
2020-05-06 03:41:35 -0500

    feat(ConfigurationModalMachine.ts): add load template and set chassis to modify context

--- docs/.vuepress/components/DriveConstants/ConfigurationModalMachine.ts
+++ docs/.vuepress/components/DriveConstants/ConfigurationModalMachine.ts
@@ -1,4 +1,9 @@
-import { Machine } from "xstate";
+import { Machine, assign } from "xstate";
+import DriveConstantStorage, {
+  StraferV1Constants,
+  BlankConstants,
+} from "./DriveConstantStorage";
+import { ChassisEnum } from "./ChassisEnum";
 
 interface ModalStateSchema {
   states: {
@@ -12,34 +17,54 @@ interface ModalStateSchema {
 }
 
 type ModalEvent =
-  | { type: "SELECTED_CHASSIS" }
-  | { type: "SELECTED_CUSTOM_CHASSIS" };
+  | { type: "SELECTED_CHASSIS"; value: ChassisEnum }
+  | { type: "SELECTED_CUSTOM_CHASSIS"; value: null };
 
 interface ModalContext {
-  chassisSelected: string;
+  chassisSelected: ChassisEnum;
 }
 
 export const configurationModalMachine = Machine<
   ModalContext,
   ModalStateSchema,
   ModalEvent
->({
-  id: "configurationModal",
-  initial: "chassisSelection",
-  context: {
-    chassisSelected: "",
+>(
+  {
+    id: "configurationModal",
+    initial: "chassisSelection",
+    context: {
+      chassisSelected: ChassisEnum.CUSTOM,
+    },
+    states: {
+      chassisSelection: {
+        on: {
+          SELECTED_CHASSIS: "done",
+          SELECTED_CUSTOM_CHASSIS: "motorSelection",
+        },
+        exit: ["setChassis", "loadTemplate"],
+      },
+      motorSelection: {},
+      wheelSelection: {},
+      driveEncoders: {},
+      botDimensions: {},
+      done: {},
+    },
   },
-  states: {
-    chassisSelection: {
-      on: {
-        SELECTED_CHASSIS: "done",
-        SELECTED_CUSTOM_CHASSIS: "motorSelection",
+  {
+    actions: {
+      setChassis: assign({
+        chassisSelected: (context, event) => event.value,
+      }),
+      loadTemplate: (context, event) => {
+        switch (event.value) {
+          case ChassisEnum.STRAFER_V1_CHASSIS:
+            DriveConstantStorage.loadTemplate(StraferV1Constants);
+            break;
+          case ChassisEnum.CUSTOM:
+            DriveConstantStorage.loadTemplate(BlankConstants);
+            break;
+        }
       },
     },
-    motorSelection: {},
-    wheelSelection: {},
-    driveEncoders: {},
-    botDimensions: {},
-    done: {},
-  },
-});
+  }
+);
