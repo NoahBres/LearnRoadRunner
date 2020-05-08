4465be02284293e1e03cc8b7c9f600ea92f14f0a
Noah <noahbresler2002@gmail.com>
2020-05-08 04:36:35 -0500

    feat(ConfigurationModal.vue): add back button, make width/height animatable for transitions

--- docs/.vuepress/components/DriveConstants/ConfigurationModal.vue
+++ docs/.vuepress/components/DriveConstants/ConfigurationModal.vue
@@ -1,19 +1,48 @@
 <template>
-  <div class="configuration-modal">
+  <div
+    class="configuration-modal flex flex-col justify-between"
+    :style="{ width: width, height: height }"
+  >
     <DriveConstants-ConfigurationModal-ChassisSelection
       v-if="this.currentState.matches('chassisSelection')"
       v-model="chassisChoice"
+      :chassisChoice="chassisChoice"
+      @request-width="requestWidth"
+      @request-height="requestHeight"
     />
 
     <DriveConstants-ConfigurationModal-MotorSelection
       v-if="this.currentState.matches('motorSelection')"
+      v-model="motorChoice"
+      :motorChoice="motorChoice"
+      @request-width="requestWidth"
+      @request-height="requestHeight"
     />
 
     <DriveConstants-ConfigurationModal-DonePage
       v-if="this.currentState.matches('done')"
+      @request-width="requestWidth"
+      @request-height="requestHeight"
     />
 
-    <div class="flex justify-end mt-12 px-8 py-4 bg-gray-200">
+    <div class="flex justify-between px-8 py-4 bg-gray-200">
+      <button
+        class="back-button flex items-center"
+        :class="{ 'translate-y-16': !backButtonShowing }"
+        @click="handleBackClick"
+      >
+        <svg
+          fill="red"
+          class="w-5 h-5 mr-2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          stroke="currentColor"
+          viewBox="0 0 24 24"
+        >
+          <path d="M10 19l-7-7m0 0l7-7m-7 7h18"></path></svg
+        >Back
+      </button>
       <button
         class="next-button"
         :class="{ 'translate-x-40': !nextButtonShowing }"
@@ -37,7 +66,10 @@ import { ChassisEnum } from "./ChassisEnum";
 export default Vue.extend({
   data() {
     return {
+      height: "400px",
+      width: "800px",
       chassisChoice: "",
+      motorChoice: "",
       configurationModalService: interpret(configurationModalMachine),
       currentState: configurationModalMachine.initialState,
       context: configurationModalMachine.context,
@@ -45,12 +77,17 @@ export default Vue.extend({
   },
   computed: {
     nextButtonShowing(): boolean {
-      if (this.currentState.matches("chassisSelection")) {
-        return this.chassisChoice;
-      } else if (this.currentState.matches("motorSelection")) {
-        return true;
-      }
+      if (this.currentState.matches("chassisSelection"))
+        return !!this.chassisChoice;
+      else if (this.currentState.matches("motorSelection"))
+        return !!this.motorChoice;
 
+      return false;
+    },
+    backButtonShowing(): boolean {
+      if (this.currentState.matches("chassisSelection")) return false;
+      else if (this.currentState.matches("motorSelection")) return true;
+      else if (this.currentState.matches("done")) return true;
       return false;
     },
   },
@@ -76,26 +113,23 @@ export default Vue.extend({
         }
       }
     },
+    handleBackClick() {
+      if (this.currentState.matches("motorSelection"))
+        this.configurationModalService.send("BACK");
+    },
+    requestWidth(value) {
+      this.width = value;
+    },
+    requestHeight(value) {
+      this.height = value;
+    },
   },
 });
 </script>
 <style lang="stylus" scoped>
-.next-button
-  @apply text-white text-base
-  @apply bg-red-600
-  @apply border-none
-  @apply px-8 py-3 rounded
-  @apply cursor-pointer
-  @apply transition-colors transition-transform ease-out duration-150 transform
 
-  transition transform 150ms cubic-bezier(.64, 1.47, .74, 1.03)
-
-  // Required because the class doesn't have enough specificity on its own
-  &.translate-x-40
-    @apply translate-x-40
-
-.next-button:hover
-  @apply bg-red-700
+.configuration-modal
+  transition width 300ms cubic-bezier(0.41, 1.3, 0.71, 1.03), height 300ms cubic-bezier(0.41, 1.3, 0.74, 1.03)
 
 .configuration-modal >>> .outline-btn
   font-family inherit
@@ -153,4 +187,37 @@ export default Vue.extend({
 .configuration-modal >>> .outline-pink[type="radio"]:checked + label
   outline-color theme("colors.pink.600")
   outline-style auto
+
+.back-button
+  @apply bg-transparent
+  @apply uppercase text-gray-600 tracking-wide
+  @apply cursor-pointer
+  @apply border-0 border-solid border-transparent border-b-2
+  @apply transition-colors transition-transform duration-150 ease-out transform
+
+  transition transform 150ms cubic-bezier(0.41, 1.3, 0.71, 1.03)
+
+  &.translate-y-16
+    @apply translate-y-16
+
+.back-button:hover
+  border-bottom-color theme("colors.gray.600")
+
+.next-button
+  @apply text-white text-base
+  @apply bg-red-600
+  @apply border-none
+  @apply px-8 py-3 rounded
+  @apply cursor-pointer
+  @apply transition-colors transition-transform ease-out duration-150 transform
+
+  transition transform 150ms cubic-bezier(0.41, 1.3, 0.71, 1.03)
+  // transition transform 150ms cubic-bezier(.67, -0.6, 0, 1.71)
+
+  // Required because the class doesn't have enough specificity on its own
+  &.translate-x-40
+    @apply translate-x-40
+
+.next-button:hover
+  @apply bg-red-700
 </style>
