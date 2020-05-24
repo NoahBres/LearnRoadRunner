1428c8a5e005a8e619525fa6c1ecc2072b9a1ce3
Noah <noahbresler2002@gmail.com>
2020-05-24 04:32:58 -0500

    feat(ManualMotorSelection.vue): Add basic manual motor selection filler

--- docs/.vuepress/components/DriveConstants/ConfigurationModal/ManualMotorSelection.vue
+++ docs/.vuepress/components/DriveConstants/ConfigurationModal/ManualMotorSelection.vue
@@ -1,3 +1,7 @@
 <template>
-  <h2>Manual Motor Selection</h2>
+  <div class="flex flex-col justify-center px-8 py-4 pb-0 h-full">
+    <h2 class="border-none mt-0 mb-10 text-center">
+      What are your motor specs?
+    </h2>
+  </div>
 </template>
