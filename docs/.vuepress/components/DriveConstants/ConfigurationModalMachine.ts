f925ef754aa6d546160f836c7d092615508c32a9
Noah <noahbresler2002@gmail.com>
2020-05-25 17:02:12 -0500

    feat(ConfigurationModalMachine.ts): add manual motor and gear ratio

--- docs/.vuepress/components/DriveConstants/ConfigurationModalMachine.ts
+++ docs/.vuepress/components/DriveConstants/ConfigurationModalMachine.ts
@@ -10,6 +10,7 @@ interface ModalStateSchema {
     chassisSelection: {};
     motorSelection: {};
     manualMotorSelection: {};
+    gearRatioSelection: {};
     wheelSelection: {};
     driveEncoders: {};
     botDimensions: {};
@@ -22,6 +23,8 @@ type ModalEvent =
   | { type: "SELECTED_CUSTOM_CHASSIS"; value: null }
   | { type: "SELECTED_MOTOR"; value: null }
   | { type: "SELECTED_CUSTOM_MOTOR"; value: null }
+  | { type: "SET_MANUAL_MOTOR"; value: null }
+  | { type: "SET_GEAR_RATIO"; value: null }
   | { type: "SELECTED_WHEEL_SIZE"; value: null }
   | { type: "BACK"; value: null };
 
@@ -50,20 +53,38 @@ export const configurationModalMachine = Machine<
       },
       motorSelection: {
         on: {
-          SELECTED_MOTOR: "wheelSelection",
+          SELECTED_MOTOR: "gearRatioSelection",
           SELECTED_CUSTOM_MOTOR: "manualMotorSelection",
           BACK: "chassisSelection",
         },
       },
       manualMotorSelection: {
         on: {
+          SET_MANUAL_MOTOR: "gearRatioSelection",
           BACK: "motorSelection",
+        },
+      },
+      gearRatioSelection: {
+        on: {
+          SET_GEAR_RATIO: "wheelSelection",
+          BACK: [
+            {
+              target: "motorSelection",
+              cond: (ctx, e, { state }) =>
+                state.history.matches("motorSelection"),
+            },
+            {
+              target: "manualMotorSelection",
+              cond: (ctx, e, { state }) =>
+                state.history.matches("manualMotorSelection"),
+            },
+          ],
         },
       },
       wheelSelection: {
         on: {
           SELECTED_WHEEL_SIZE: "botDimensions",
-          BACK: "motorSelection",
+          BACK: "gearRatioSelection",
         },
       },
       botDimensions: {
