64fbf3a605e604212fef0119c0db2d1216fdd7c8
Noah <noahbresler2002@gmail.com>
2020-05-29 22:59:53 -0500

    feat(AyudeSelection.vue): create an ayude selection

---
+++ docs/.vuepress/components/DriveConstants/ConfigurationModal/AyudeSelection.vue
@@ -0,0 +1,23 @@
+<template>
+  <div
+    class="flex flex-col justify-center items-center px-16 py-12 pb-0 h-full"
+  >
+    <Ayude @input="$emit('input', $event)" />
+    <p class="max-w-sm mt-6">
+      Not sure what if you're using Drive Encoders? Check out the
+      <a href="/quickstart-overview.html#are-you-using-drive-encoders"
+        >quickstart overview</a
+      >.
+    </p>
+  </div>
+</template>
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  mounted() {
+    this.$emit("request-width", "512px");
+    this.$emit("request-height", "414px");
+  },
+});
+</script>
