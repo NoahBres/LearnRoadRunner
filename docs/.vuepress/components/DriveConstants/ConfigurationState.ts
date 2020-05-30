34aadb214e38116351091574977c9ed13dc7874f
Noah <noahbresler2002@gmail.com>
2020-05-30 02:59:02 -0500

    feat(ConfigurationStates.ts): create a ConfigurationState interface

---
+++ docs/.vuepress/components/DriveConstants/ConfigurationState.ts
@@ -0,0 +1,25 @@
+import { Motor, MotorGroupItem } from "./MotorData";
+
+enum ChassisEnum {
+  STRAFER_V1_CHASSIS = "STRAFER_V1_CHASSIS",
+  CUSTOM = "CUSTOM",
+}
+
+interface ConfigurationState {
+  chassisSelected: ChassisEnum;
+
+  customMotorSelected: boolean;
+  motorGroupSelected: MotorGroupItem;
+  motorSelected: Motor;
+
+  ticksPerRev: number;
+  maxRPM: number;
+
+  gearRatio: number;
+  wheelRadius: number;
+  trackWidth: number;
+
+  runUsingEncoder: boolean;
+}
+
+export { ConfigurationState as default, ChassisEnum };
