5820b7a7762d6af61679b29f6d505a820fb53155
Noah Bresler <noahbresler2002@gmail.com>
2020-08-10 22:34:09 -0500

    feat(TuningSimulator.vue): create tuning simulator component

---
+++ docs/.vuepress/components/VeloPIDTuning/TuningSimulator.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <div class="bg-blue-200" :style="{ height: graphHeight }"></div>
+    <div class="bg-green-200 h-4"></div>
+  </div>
+</template>
+<script lang="ts">
+import Vue from "vue";
+import uPlot from "uplot";
+
+export default Vue.extend({
+  props: {
+    graphHeight: {
+      type: String,
+      default: "30rem",
+    },
+  },
+  data() {
+    return {};
+  },
+  mounted() {},
+});
+</script>
+<style src="uplot/dist/uPlot.min.css"></style>
