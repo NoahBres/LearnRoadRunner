2d9c00b78a4c6890c6f0abaa3b1fd72468bd5f9d
Noah Bresler <noahbresler2002@gmail.com>
2020-08-13 06:29:46 -0500

    feat(TuningSimulator.vue): implement base motor dynamics

--- docs/.vuepress/components/VeloPIDTuning/TuningSimulator.vue
+++ docs/.vuepress/components/VeloPIDTuning/TuningSimulator.vue
@@ -41,21 +41,37 @@ export default Vue.extend({
       paddingX: -0,
       paddingY: -40,
 
-      // Velocity stuff
+      // Target Velocity
       maxDataLength: 200,
 
       maxVel: 50,
       maxAccel: 50,
       coastingLength: 1000, // in ms
 
-      maxVelPadding: 5,
+      maxVelPadding: 20,
 
-      currentVelo: 0,
+      targetVelocity: 0,
 
       startTime: 0,
       lastLoopTime: 0,
 
       coastStart: 0,
+
+      // Real Velocity
+      currentVelocity: 0,
+
+      kP: 0,
+      kI: 0,
+      kD: 0,
+      kV: 0.0153,
+      kS: 0.002,
+
+      setVoltage: 2,
+
+      // fake motor model??
+      modelKV: 0.0153,
+      modelKA: 0.002,
+      modelKStatic: 0.0021,
 
       lastState: GraphState.Accel as GraphState,
       currentState: GraphState.Accel as GraphState,
@@ -81,6 +97,7 @@ export default Vue.extend({
         {
           label: "velocity0",
           scale: "%",
+          width: 2,
           stroke: "red",
         },
       ],
@@ -154,21 +171,22 @@ export default Vue.extend({
 
       const timeDelta = (currentTime - this.lastLoopTime) / 1000;
 
+      // Target velocity
       switch (this.currentState) {
         case GraphState.Accel:
-          this.currentVelo = Math.min(
-            this.currentVelo + this.maxAccel * timeDelta,
+          this.targetVelocity = Math.min(
+            this.targetVelocity + this.maxAccel * timeDelta,
             this.maxVel
           );
 
-          if (this.currentVelo >= this.maxVel) {
+          if (this.targetVelocity >= this.maxVel) {
             this.coastStart = currentTime;
 
             this.lastState = GraphState.Accel;
             this.currentState = GraphState.Coast;
           }
 
-          this.graphData[1].push(this.currentVelo);
+          this.graphData[1].push(this.targetVelocity);
           break;
         case GraphState.Coast:
           if (currentTime - this.coastStart >= this.coastingLength) {
@@ -180,26 +198,43 @@ export default Vue.extend({
             this.lastState = GraphState.Coast;
           }
 
-          this.graphData[1].push(this.currentVelo);
+          this.graphData[1].push(this.targetVelocity);
 
           break;
         case GraphState.Decel:
-          this.currentVelo = Math.max(
-            this.currentVelo - this.maxAccel * timeDelta,
+          this.targetVelocity = Math.max(
+            this.targetVelocity - this.maxAccel * timeDelta,
             -this.maxVel
           );
 
-          if (this.currentVelo <= -this.maxVel) {
+          if (this.targetVelocity <= -this.maxVel) {
             this.coastStart = currentTime;
 
             this.lastState = GraphState.Decel;
             this.currentState = GraphState.Coast;
           }
 
-          this.graphData[1].push(this.currentVelo);
+          this.graphData[1].push(this.targetVelocity);
 
           break;
       }
+
+      // Current Velocity
+      this.currentVelocity +=
+        ((-this.modelKV / this.modelKA) * this.currentVelocity +
+          (1 / this.modelKA) * this.setVoltage) *
+        timeDelta;
+
+      this.currentVelocity = Math.min(this.currentVelocity, this.maxVel * 1.3);
+
+      // Actual controller stuff
+
+      this.setVoltage =
+        this.kV * this.targetVelocity +
+        this.kS +
+        (this.targetVelocity - this.currentVelocity) * this.kP;
+
+      this.graphData[2].push(this.currentVelocity);
 
       // Assume that all the graphData nested arrays are of equal size
       // Limit window of the graph
