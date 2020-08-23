bdb20099daecdc5ca5b1bdd26ba90673702bfd3c
Noah Bresler <noahbresler2002@gmail.com>
2020-08-22 22:10:11 -0500

    feat(VideoDisplay.vue): add more option to the VideoDisplay component

--- docs/.vuepress/components/VideoDisplay.vue
+++ docs/.vuepress/components/VideoDisplay.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="py-4 flex justify-center" :width="width">
-    <video width="100%" :controls="controls" autoplay loop muted playsinline>
+    <video
+      width="100%"
+      :controls="controls"
+      v-bind:autoplay="autoplay"
+      :loop="loop"
+      :muted="muted"
+      :playsinline="playsinline"
+    >
       <source :src="src" type="video/mp4" />
       Your browser does not support the video tag 😢.
     </video>
@@ -11,9 +18,29 @@ export default {
   props: {
     src: {
       type: String,
+      default: "",
+    },
+    width: {
+      type: String,
       default: "100%",
     },
     controls: {
+      type: Boolean,
+      default: true,
+    },
+    muted: {
+      type: Boolean,
+      default: true,
+    },
+    autoplay: {
+      type: Boolean,
+      default: true,
+    },
+    loop: {
+      type: Boolean,
+      default: true,
+    },
+    playsinline: {
       type: Boolean,
       default: true,
     },
