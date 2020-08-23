0b736180b22ad86bbadb38eec3cd33d22320d786
Noah Bresler <noahbresler2002@gmail.com>
2020-08-22 22:03:13 -0500

    feat(VideoDisplay.vue): add explicit width and autoplay/loop/controls to video component

--- docs/.vuepress/components/VideoDisplay.vue
+++ docs/.vuepress/components/VideoDisplay.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="py-4 flex justify-center">
-    <video width="100%" controls>
+  <div class="py-4 flex justify-center" :width="width">
+    <video width="100%" :controls="controls" autoplay loop muted playsinline>
       <source :src="src" type="video/mp4" />
       Your browser does not support the video tag 😢.
     </video>
@@ -8,6 +8,15 @@
 </template>
 <script>
 export default {
-  props: ["src"],
+  props: {
+    src: {
+      type: String,
+      default: "100%",
+    },
+    controls: {
+      type: Boolean,
+      default: true,
+    },
+  },
 };
 </script>
