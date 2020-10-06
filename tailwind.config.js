9bfb34615cc03d289f58346660d96b42e870e19b
Noah Bresler <noahbresler2002@gmail.com>
2020-10-06 15:32:45 -0500

    fix(tailwind.config.js): disgusting hacking increasing desktop breakpoint to appease 1280px screens

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,16 @@ module.exports = {
       opacity: {
         "85": "0.85",
       },
+      screens: {
+        tablet: "640px",
+        // => @media (min-width: 640px) { ... }
+
+        laptop: "1024px",
+        // => @media (min-width: 1024px) { ... }
+
+        desktop: "1285px",
+        // => @media (min-width: 1280px) { ... }
+      },
     },
   },
   variants: {},
