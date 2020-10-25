d6dab8d663ac2969bbbc2a7081d46e9736407c9d
Noah Bresler <noahbresler2002@gmail.com>
2020-10-25 05:26:08 -0500

    feat(config.js): add opengraph tags

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -7,15 +7,29 @@ module.exports = {
       { name: "viewport", content: "width=device-width, initial-scale=1" },
     ],
     ["meta", { charset: "utf-8" }],
-    ['link', { rel: "apple-touch-icon", sizes: "180x180", href: "/assets/favicons/apple-touch-icon.png"}],
-    ['link', { rel: "icon", type: "image/png", sizes: "32x32", href: "/assets/favicons/favicon-32x32.png"}],
-    ['link', { rel: "icon", type: "image/png", sizes: "16x16", href: "/assets/favicons/favicon-16x16.png"}],
-    ['link', { rel: "manifest", href: "/assets/favicons/site.webmanifest"}],
-    ['link', { rel: "mask-icon", href: "/assets/favicons/safari-pinned-tab.svg", color: "#3a0839"}],
-    ['link', { rel: "shortcut icon", href: "/assets/favicons/favicon.ico"}],
-    ['meta', { name: "msapplication-TileColor", content: "#3a0839"}],
-    ['meta', { name: "msapplication-config", content: "/assets/favicons/browserconfig.xml"}],
-    ['meta', { name: "theme-color", content: "#1e429f"}]
+    
+    // App icons
+    ['link', { rel: "apple-touch-icon", sizes: "180x180", href: "/assets/favicons/apple-touch-icon.png" }],
+    ['link', { rel: "icon", type: "image/png", sizes: "32x32", href: "/assets/favicons/favicon-32x32.png" }],
+    ['link', { rel: "icon", type: "image/png", sizes: "16x16", href: "/assets/favicons/favicon-16x16.png" }],
+    ['link', { rel: "manifest", href: "/assets/favicons/site.webmanifest" }],
+    ['link', { rel: "mask-icon", href: "/assets/favicons/safari-pinned-tab.svg", color: "#3a0839" }],
+    ['link', { rel: "shortcut icon", href: "/assets/favicons/favicon.ico" }],
+    ['meta', { name: "msapplication-TileColor", content: "#3a0839" }],
+    ['meta', { name: "msapplication-config", content: "/assets/favicons/browserconfig.xml" }],
+    ['meta', { name: "theme-color", content: "#1e429f" }],
+    
+    // Open Graph tags
+    ['meta', { property: "og:type", content: "article" }],
+    ['meta', { property: "og:title", content: "Learn Road Runner" }],
+    ['meta', { property: "og:description", content: "Level up your auto. Road Runner made easy." }],
+    ['meta', { property: "og:image", content: "/assets/baby/baby-rr-150ppi.png" }],
+
+    // Twitter card
+    ['meta', { property: "twitter:card", content: "summary" }],
+    ['meta', { property: "twitter:title", content: "Vue.js" }],
+    ['meta', { property: "twitter:description", content: "FTC's Road Runner library made easy" }],
+    ['meta', { property: "twitter:image", content: "/assets/baby/baby-rr-150ppi.png"}]
   ],
   themeConfig: {
     logo: '/assets/baby/baby-rr.svg',
